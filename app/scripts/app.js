41bbe902d7b8614a9006053ea5209bd7b5839ae2
Marco Pivetta <ocramius@gmail.com>
2013-07-17 01:24:34 +0200

    Adding sample repositories view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('ocramius.github.comApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller:  'MainCtrl'
+            })
+            .when('/repositories', {
+                templateUrl: 'views/repositories.html',
+                controller:  'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('ocramius.github.comApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.repositories = [
+            {name: "OcraServiceManager", stars: 1234, forks: 12},
+            {name: "OcraDiCompiler", stars: 4567, forks: 34},
+            {name: "ProxyManager", stars: 8900, forks: 56}
+        ];
+    });
