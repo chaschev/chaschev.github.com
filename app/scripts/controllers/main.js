ecae1232891ae6b0d9a1d52456948a322f915285
Marco Pivetta <ocramius@gmail.com>
2013-07-22 20:44:40 +0200

    Committing partially working state

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,21 @@
 
 angular.module('ocramius.github.comApp')
     .controller('MainCtrl', function ($scope, githubService) {
-        $scope.awesomeThings = githubService.awesomeThings;
-        $scope.repositories = githubService.repositories;
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+        $scope.github = githubService;
+        console.log(githubService);
+
+        var repo = githubService.getRepo('Ocramius', 'ProxyManager');
+        var user = githubService.getUser('Ocramius');
+
+        var orgs = user.orgs(function (err, orgs) {
+            console.log({orgs: orgs});
+        });
+
+        console.log(repo);
     });
--- app/scripts/services/githubService.js
+++ app/scripts/services/githubService.js
@@ -3,17 +3,7 @@ angular.module('ocramius.github.comApp')
     .factory('githubService', function(){
         //return require('github');
 
-        return {
-           awesomeThings : [
-               'HTML5 Boilerplate',
-               'AngularJS',
-               'Karma'
-           ],
-
-            repositories : [
-                {name: "OcraServiceManager", stars: 1234, forks: 12},
-                {name: "OcraDiCompiler", stars: 4567, forks: 34},
-                {name: "ProxyManager", stars: 8900, forks: 56}
-            ]
-        };
+        return new Github({
+            auth: 'none'
+        });
     });
